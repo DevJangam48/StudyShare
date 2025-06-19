acb29a4d0b0e225dc5a09294efaded9ae6f02244
Devjangam48 <djangam4805@gmail.comgit config --global user.name DevJangam48>
2025-06-19 22:22:00 +0530

    Updated the socket url

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
+const BACKEND_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
-const socket = io("http://localhost:5000", {
+const socket = io(BACKEND_URL, {
   withCredentials: true,
 });
 
